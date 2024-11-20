f26d7e286d05f11eb34b1e36e5103f04c844200b
Diego Borrallo Herrero <diegobh@usal.es>
2024-11-20 13:04:08 +0100

    cambios cliente TAM_BUFFER

--- codigo/cliente.c
+++ codigo/cliente.c
@@ -43,9 +43,9 @@ char *argv[];
     char hostname[MAXHOST] = "localhost";
 	char buf[TAM_BUFFER];
     char peticion_TCP[TAM_BUFFER];
-    char peticion_UDP[BUFFERSIZE];
+    char peticion_UDP[TAM_BUFFER]; // BUFFERSIZE
     char respuesta_TCP[TAM_BUFFER];
-    char respuesta_UDP[BUFFERSIZE];
+    char respuesta_UDP[TAM_BUFFER]; // BUFFERSIZE
 
     if (argc > 3) {
         fprintf(stderr, "Usage: %s TCP/UDP [usuario[@host]]\n", argv[0]);
@@ -216,9 +216,9 @@ char *argv[];
         while (n_retry > 0) {
             /* Build the request based on input arguments */
             if (argc == 2) {
-                snprintf(peticion_UDP, BUFFERSIZE, "\r\n");
+                snprintf(peticion_UDP, TAM_BUFFER, "\r\n"); //BUFFERSIZE
             } else {
-                snprintf(peticion_UDP, BUFFERSIZE, "%s\r\n", argv[2]);
+                snprintf(peticion_UDP, TAM_BUFFER, "%s\r\n", argv[2]); //BUFFERSIZE
             }        
             if (sendto(s, peticion_UDP, strlen(peticion_UDP), 0, (struct sockaddr *)&servaddr_in, sizeof(struct sockaddr_in)) == -1) {
                 perror(argv[0]);
