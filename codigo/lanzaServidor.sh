e37ad4bee35fb7a8964e3e11bc6f7294259aab89
Diego Borrallo Herrero <diegobh@usal.es>
2024-12-07 18:03:10 +0100

    prueba lanzaservidor

--- codigo/lanzaServidor.sh
+++ codigo/lanzaServidor.sh
@@ -2,12 +2,12 @@
 # Lanza el servidor que es un daemon y varios clientes
 ./servidor
 ./cliente TCP &
-./cliente TCP David &
+./cliente TCP diegobh &
 ./cliente TCP @nogal.usal.es &
 ./cliente TCP zxcvb@nogal.usal.es &
 ./cliente TCP p1777001@nogal.usal.es &
 ./cliente UDP &
-./cliente UDP david &
+./cliente UDP diegobh &
 ./cliente UDP @nogal.usal.es &
 ./cliente UDP zxcvb@nogal.usal.es &
 ./cliente UDP p1777001@nogal.usal.es &
